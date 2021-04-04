263b71e92e4c49bf8dde21e5e5c53955fda36aa4
AninossII <aninosspro@gmail.com>
2021-04-04 23:14:44 +0100

    Update Action add Ajout method, make New Event page and Update home page spon Event's

---
+++ StagPj/Home.cs
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StagPj
+{
+    public class Home
+    {
+    }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StagPj
+{
+    public class Home
+    {
+    }
+>>>>>>> 1469bfff1b6108f8ac1ea9a1d0122f6c974317ec
+}
---
+++ StagPj/Program.cs
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagPj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+=======
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagPj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+>>>>>>> 1469bfff1b6108f8ac1ea9a1d0122f6c974317ec
