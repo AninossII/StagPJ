d7998f5152402ca6268afa518dd922bf3e465afb
AninossII <aninosspro@gmail.com>
2021-04-05 11:05:21 +0000

    Update all Bugs in this stage

--- StagPj/Home.cs
+++ StagPj/Home.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +8,4 @@ namespace StagPj
     public class Home
     {
     }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace StagPj
-{
-    public class Home
-    {
-    }
->>>>>>> 1469bfff1b6108f8ac1ea9a1d0122f6c974317ec
 }
