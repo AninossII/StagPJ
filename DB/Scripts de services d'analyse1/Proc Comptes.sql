543e9b74994334b812b78227d2f2e8439f8d9ae4
AyoubSmina <01.saayoub@gmail.com>
2021-04-09 23:19:31 +0100

    Mode Update

--- DB/Scripts de services d'analyse1/Proc Comptes.sql
+++ DB/Scripts de services d'analyse1/Proc Comptes.sql
@@ -12,14 +12,19 @@ create or alter procedure dbo.I_Comptes
 as 
 SET NOCOUNT ON
 BEGIN TRY
-    INSERT INTO dbo.Comptes(Nom,C_Montant,U_id) 
-	values (@Nom,@C_Montant,@U_id)
-
-	if (Select @@CONNECTIONS) > 0
-		throw 50500,'Success',1;
+	if (select count(Nom) from Comptes where Nom = @Nom and U_id = @U_id) > 0
+		begin
+			set @responseMessage = '50404##Le nom de compte deja existe';
+		end
 	else
-		throw 50404,'Faild',1;
-
+		begin
+		INSERT INTO dbo.Comptes(Nom,C_Montant,U_id) 
+		values (@Nom,@C_Montant,@U_id)
+		if (Select @@CONNECTIONS) > 0
+			throw 50500,'Success',1;
+		else
+			throw 50404,'Faild',1;
+	end
 END TRY
 BEGIN CATCH
     SET @responseMessage= concat(ERROR_NUMBER(),'##',ERROR_MESSAGE())
@@ -39,16 +44,21 @@ create or alter procedure dbo.U_Comptes
 @responseMessage char(256) OUTPUT
 as
 BEGIN TRY
-
+	if (select count(Nom) from Comptes where Nom = @Nom and U_id = @U_id) > 0
+		begin
+			set @responseMessage = '50404##Le nom de compte deja existe';
+		end
+	else
+	begin
 	update Comptes 
 	set Nom = @Nom,C_Montant = @C_Montant,U_id = @U_id
 	where id = @ID
     
-	if (Select @@CONNECTIONS) > 0
-		throw 50500,'Success',1;
-	else
-		throw 50404,'Faild',1;
-
+			if (Select @@CONNECTIONS) > 0
+				throw 50500,'Success',1;
+			else
+				throw 50404,'Faild',1;
+	end
 END TRY
 BEGIN CATCH
     SET @responseMessage=concat(ERROR_NUMBER(),'##',ERROR_MESSAGE())
@@ -81,3 +91,70 @@ declare @message char(256)
 
 print @message
 -------------------------
+create or alter function Get_name_from_ID(@ID uniqueidentifier)
+returns char(20)
+begin
+return (select Nom from comptes where ID = @ID)
+end
+
+select dbo.Get_name_from_ID('0F475268-396E-4374-9147-0E3DF0E155DC')
+select * from Comptes
+--------------------------------------------------------------------
+CREATE OR ALTER function [dbo].[Get_ID_from_nom_U_ID](@Nom char(50), @U_id uniqueidentifier)
+returns uniqueidentifier
+begin
+return (select ID from comptes where Nom = @Nom AND U_id =@U_id)
+end
+
+SELECT dbo.Get_ID_from_nom_U_ID('CIH','CDFA2082-41C6-42D2-B82E-68D0643799D4')
+--------------------------------------------------------------------
+----- To add montant to compte
+create or alter procedure Add_Money
+@id_C uniqueidentifier,
+@id_U uniqueidentifier,
+@Montant float,
+@responseMessage char(256) OUTPUT
+as
+BEGIN TRY
+	update Comptes
+	set C_Montant += @Montant 
+	where ID = @id_C and U_id = @id_U
+END TRY
+BEGIN CATCH
+    SET @responseMessage=concat(ERROR_NUMBER(),'##',ERROR_MESSAGE())
+END CATCH
+
+declare @msg char(100)
+exec dbo.Add_Money 'E64A4B93-B5E1-4D40-ABC3-2CDCDF229E6D','D7232F9B-22E1-44B1-9019-BF2CFBB4D497',500.1,@msg out
+print concat('dddddddd',@msg,'vvvvvvvvvv')
+-----------------------------------------------------------------------------
+
+create or alter procedure Withdraw_Money_from_compte
+@id_C uniqueidentifier,
+@id_U uniqueidentifier,
+@Montant float,
+@responseMessage char(256) OUTPUT
+as
+BEGIN TRY
+	if((select C_Montant from Comptes where ID = @id_C and U_id = @id_U	) - @Montant)>=0
+	begin 
+		update Comptes
+		set C_Montant -= @Montant 
+		where ID = @id_C and U_id = @id_U	
+	end
+	else
+	begin 
+		 SET @responseMessage = '50404##You don`t have money enough!!';
+	end
+END TRY
+BEGIN CATCH
+    SET @responseMessage=concat(ERROR_NUMBER(),'##',ERROR_MESSAGE())
+END CATCH
+
+
+declare @msg char(100)
+exec dbo.Withdraw_Money_from_compte 'E64A4B93-B5E1-4D40-ABC3-2CDCDF229E6D','D7232F9B-22E1-44B1-9019-BF2CFBB4D497',10,@msg out
+print @msg
+
+
+select * from Comptes
--- DB/Scripts de services d'analyse1/Sql_Triggers.sql
+++ DB/Scripts de services d'analyse1/Sql_Triggers.sql
@@ -1,4 +1,5 @@
 ------------------------Triggers For Utilisateur-----------------------------------------------------
+---------------TO add Default Comptes
 create or alter Trigger Creation_Compte_Par_Default 
 on Utilisateur 
 after insert
@@ -8,19 +9,44 @@ if(select count(*) from inserted)=1
 		declare @id uniqueidentifier =(select top(1) ID from inserted)
 		insert into Comptes(Nom,U_id) values ('Jibi',@id),('Note',@id)
 	end
-
-
-
-
-
-
-
+---------------TO 
 
 
 ------------------------Triggers For Compte-----------------------------------------------------
+---------------TO 
+/*create or alter Trigger For_not_repeate_nom_Compte 
+on Comptes 
+after Insert,update
+as
+declare @t cursor select nom from  
 
+declare @NOM char(20) = (Select top(1) Nom from inserted)
+declare @U_ID UNIQUEIDENTIFIER = (Select top(1) U_id from inserted)
+	if (select Count(Nom) from Comptes where U_id = @U_ID and Nom = @NOM group by U_id) > 0 
+	BEGIN 
+		PRINT 'THIS IS REPETATE'
+		rollback
+	END 
+Go
+
+*/
 ------------------------Triggers For Action-----------------------------------------------------
 
 ------------------------Triggers For Utilisateur-----------------------------------------------------
+
+declare @msg char(100)
+exec dbo.I_Comptes 'CI',50,'D7232F9B-22E1-44B1-9019-BF2CFBB4D497',@msg out
+print trim(@msg)
+
+Select * from Comptes order by Nom
+
+delete from comptes where u_id = 'D7232F9B-22E1-44B1-9019-BF2CFBB4D497' and C_montant = 50
+
+select Count(Nom) from Comptes where Nom = 'Note' AND U_id = 'D7232F9B-22E1-44B1-9019-BF2CFBB4D497'
+
+declare @msg char(100)
+exec dbo.U_Comptes '16FC574C-CF16-4FF4-A4BE-4B9A0E8986AB','Cii',50,'D7232F9B-22E1-44B1-9019-BF2CFBB4D497',@msg out
+print trim(@msg)
+
 
 
