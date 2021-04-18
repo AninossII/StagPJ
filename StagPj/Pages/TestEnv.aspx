9caf231f70a00a08dfcf277047d09c4b682b50cf
AninossII <aninosspro@gmail.com>
2021-04-18 17:00:17 +0000

    Done the Category Systeme

--- StagPj/Pages/TestEnv.aspx
+++ StagPj/Pages/TestEnv.aspx
@@ -5,15 +5,26 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        #tags {
+            width: 161px;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-        <input ID="TextBox1" runat="server" list="dlCategory"></input>
+        </input>
         
-        <div>
+        &nbsp;<div>
            
+            <div class="bootstrap-tagsinput"> 
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
         </div>
         
+
+        <p>
+            <input id="tags" runat="server" size="1" type="text" data-role="tagsinput" list="catyList" /></p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
     </form>
 </body>
 </html>
--- StagPj/Pages/TestEnv.aspx.cs
+++ StagPj/Pages/TestEnv.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -14,20 +16,74 @@ namespace StagPj.Pages
         private Connexion con;
         private SqlCommand cmd;
         private Utilisateur u;
+        private Action a;
 
-        private DataList dl;
+        private DataTable dataTable;
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            a = new Action();
             u = new Utilisateur();
             con = new Connexion();
 
-            u.Email = Request.Cookies["logIn"]["Email"];
-            u.Password = Request.Cookies["logIn"]["Password"];
-            u.LogIn();
+            if (u.ID != null)
+            {
 
+            }
+            else if (Request.Cookies["logIn"] != null)
+            {
+                u.Email = Request.Cookies["logIn"]["Email"];
+                u.Password = Request.Cookies["logIn"]["Password"];
+                u.LogIn();
+            }
+            else if (u.ID == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
 
+            dataTable = new DataTable();
+
+            con = new Connexion();
+
+            DataTable cTable = con.showParamDataTable("dbo.Get_Comptes_from_ID_Utili", "@ID");
+
+            DataTable actionTable = new DataTable();
+
+            int i = 0;
+
+            Label1.Controls.Add(new LiteralControl("<div>"));
+            Label1.Controls.Add(new LiteralControl("<datalist id=" + "catyList" + ">"));
+
+            foreach (DataRow cRaw in cTable.Rows)
+            {
+                actionTable = con.showDataTable("select * from dbo.action" + " where C_id = '" + cRaw["ID"] + "'");
+
+                foreach (DataRow aRow in actionTable.Rows)
+                {
+                    actionTable = con.showDataTable("select * from dbo.categorie where A_id = '" + aRow["ID"] + "'");
+
+                    if(actionTable.Rows.Count != 0)
+                    {
+                        Label1.Controls.Add(new LiteralControl("<option>"));
+
+                        var Tname = new Label();
+                        Tname.Text = actionTable.Rows[0][1].ToString();
+                        Label1.Controls.Add(Tname);
+
+                        Label1.Controls.Add(new LiteralControl("</option>"));
+                    }
+                }
+               
+            }
+
+            Label1.Controls.Add(new LiteralControl("</datalist>"));
+            Label1.Controls.Add(new LiteralControl("</div>"));
         }
-
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write(tags.Value);
+        }
     }
 }
